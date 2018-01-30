a54b2cbebad018cc7bb28adc98d949a8eee21e17
Todd Day <tday44@users.noreply.github.com>
2018-01-30 10:35:25 -0700

    First Commit

---
+++ autocomplete_search.php
@@ -0,0 +1,23 @@
+<?php
+$host = null;
+$socket = "/cloudsql/webautocomplete-182600:us-central1:giwac-sql-large"; /* Host name */
+$user = "root"; /* User */
+$password = "warfield"; /* Password */
+$dbname = "giwacdb"; /* Database name */
+
+$searchTerm = $_GET['term']; 	//get search term
+
+//connect with the database
+$db = mysqli_connect($host, $user, $password, $dbname, $port, $socket);
+if (!$db) {
+	die("Connection failed: " . mysqli_connect_error());
+}else{
+	$query = "SELECT name FROM products WHERE name LIKE '%".$searchTerm."%' ORDER BY name ASC Limit 0,25";
+	$result = mysqli_query($db,$query);
+	while ($row = mysqli_fetch_array($result)){
+		$data[] = $row['name'];
+	}
+}
+//return json data
+echo json_encode($data);
+?>
